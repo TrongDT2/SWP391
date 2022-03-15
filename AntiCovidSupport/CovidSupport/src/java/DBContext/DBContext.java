7fcb1af04d133c5183211b9291ccaf7409d6e06d
RuaCon74 <longnthe140165@fpt.edu.vn>
2022-03-15 16:47:02 +0700

    Update DBContext.java

--- AntiCovidSupport/CovidSupport/src/java/DBContext/DBContext.java
+++ AntiCovidSupport/CovidSupport/src/java/DBContext/DBContext.java
@@ -16,14 +16,7 @@ import java.util.logging.Logger;
 public class DBContext {
 
     private String HOST = "localhost";
-
-
-
-    private String PORT = "1433";
-
-
     private String PORT = "58560";
-
     private String DATABASE_NAME = "CovidSP";
     private String USERNAME = "sa";
     private String PASSWORD = "123456";
